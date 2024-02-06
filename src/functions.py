04a2426611258d2bc3ef5d7f0fe202c5c1829d24
Haris Felić <138762227+harifel@users.noreply.github.com>
2024-02-06 21:20:33 +0100

    Update functions.py
    Update

--- src/functions.py
+++ src/functions.py
@@ -9,14 +9,14 @@ def plotting_raw_data(X, y, alpha, s, color, label, grid, axes, plot_columns_x_l
     axes[0].scatter(y, X.iloc[:, 0], alpha=alpha, s=s, c=color, label=label)
     axes[0].set_ylabel(plot_columns_x_label[0])
     if grid:
-        axes[0].grid()
+        axes[0].grid(linestyle = 'dotted', linewidth = 0.3)
     axes[0].legend(loc='upper right')
 
     # Second subplot
     axes[1].scatter(y, X.iloc[:, 1], alpha=alpha, s=s, c=color, label=label)
     axes[1].set_ylabel(plot_columns_x_label[1])
     if grid:
-        axes[1].grid()
+        axes[1].grid(linestyle = 'dotted', linewidth = 0.3)
     axes[1].legend(loc='upper right')
 
     # Third subplot
@@ -24,7 +24,7 @@ def plotting_raw_data(X, y, alpha, s, color, label, grid, axes, plot_columns_x_l
     axes[2].set_ylabel(plot_columns_x_label[2])
     axes[2].legend(loc='upper right')
     if grid:
-        axes[2].grid()
+        axes[2].grid(linestyle = 'dotted', linewidth = 0.3)
 
     # # Fourth subplot
     axes[3].scatter(y, X.iloc[:, 3], alpha=alpha, s=s, c=color, label=label)
@@ -32,12 +32,10 @@ def plotting_raw_data(X, y, alpha, s, color, label, grid, axes, plot_columns_x_l
     axes[3].set_ylabel(plot_columns_x_label[3])
     axes[3].legend(loc='upper right')
     if grid:
-        axes[3].grid()
+        axes[3].grid(linestyle = 'dotted', linewidth = 0.3)
 
     # Adjust layout to prevent overlapping
     plt.tight_layout()
-
-
 
 
 
@@ -94,11 +92,6 @@ def plot_cpt_data(figsize, plot_columns_x, df_raw, df_SCPTu_SCPT, id_value, plot
 
     df_id = df_SCPTu_SCPT.loc[df_raw.loc[:,'ID'] == id_value]
     for i, column in enumerate(plot_columns_x[1:-1]):
-        # Plot measured data
-        # axes[i].plot(df_id[column].values,
-        #               df_id[plot_columns_x[0]].values,
-        #               label=f'Raw data (CPT ID {id_value})',
-        #               marker='o', color='k', linewidth=0.5, markersize=2)
 
         axes[i].plot(df_id[column+"_mean"].values,
                       df_id[plot_columns_x[0]].values,
@@ -111,10 +104,10 @@ def plot_cpt_data(figsize, plot_columns_x, df_raw, df_SCPTu_SCPT, id_value, plot
 
         axes[0].set_ylabel(plot_columns_x_label[0])
         axes[i].set_xlabel(plot_columns_x_label[i+1])
-        axes[i].grid(True, which='both')
+        axes[i].grid(True, which='both', linestyle = 'dotted', linewidth = 0.3)
         axes[i].minorticks_on()
 
-    axes[0].legend(loc='upper center', bbox_to_anchor=(2.3, 1.15), ncol=2, handlelength = 3)
+    axes[0].legend(loc='upper center', bbox_to_anchor=(2.3, 1.15), ncol=2, handlelength = 2, labelspacing=2)
 
     # Use a different variable for the last subplot
     last_subplot_label = plot_columns_x_label[-1]
@@ -145,18 +138,9 @@ def plot_cpt_data_NW_site(figsize, plot_columns_x, df_site, df_smoothed, df_proc
         axes[i].set_xlim(xmin=0)
 
         axes[i].set_xlabel(plot_columns_x_label[i+1])
-        #axes[i].set_ylabel('Depth [m]')
-        axes[i].grid(True, which='both')
-        #axes[i].legend()
+        axes[i].grid(True, which='both', linestyle = 'dotted', linewidth = 0.3)
         axes[i].minorticks_on()
-        #axes[i].invert_yaxis()
 
-    # for i, column in enumerate(plot_columns_x[1:-1]):
-    #     # Plot measured data
-    #     axes[i].plot(df_smoothed[column].values,
-    #                   df_smoothed[plot_columns_x[0]].values,
-    #                   #label='Smoothed data',
-    #                   marker='o', color='k', linewidth = 0.4, markersize=2)
 
     for i, column in enumerate(plot_columns_x[1:-1]):
         # Plot measured data
@@ -165,19 +149,9 @@ def plot_cpt_data_NW_site(figsize, plot_columns_x, df_site, df_smoothed, df_proc
                       label='Input ML',
                       marker='o', color='r', linewidth = 0.5, markersize=2)
 
-        # axes[i].set_ylim(ymin=0)
-        # axes[i].set_xlim(xmin=0)
-
-        #axes[i].set_xlabel(plot_columns_x_label[i])
-        #axes[i].set_ylabel('Depth [m]')
-        # axes[i].grid(True, which='both')
         axes[i].legend(loc='lower center')
-        # axes[i].minorticks_on()
-        #axes[i].invert_yaxis()
 
     axes[0].set_ylabel('Depth [m]')
-
-    #axes[2].legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=2)
 
     axes[-1].plot(y_true,
                   df_proccessed[plot_columns_x[0]].values,
@@ -189,16 +163,63 @@ def plot_cpt_data_NW_site(figsize, plot_columns_x, df_site, df_smoothed, df_proc
                 marker='o', color='blue', linewidth = 0.5, markersize=2)
 
     axes[-1].set_xlabel(plot_columns_x_label[-1])
-    #axes[-1].set_ylabel('Depth [m]')
-    axes[-1].grid(True, which='both')
-    axes[-1].legend(loc='lower center')
+    axes[-1].grid(True, which='both', linestyle = 'dotted', linewidth = 0.3)
+    axes[-1].legend(loc='lower center', handlelength=2, labelspacing=1)
+    axes[-1].minorticks_on()
+    axes[-1].invert_yaxis()
+    axes[-1].set_xlim(xmin=0)
+
+    plt.tight_layout()
+    return fig, axes
+
+
+
+
+def plot_cpt_data_NW_site_all(figsize, plot_columns_x, df_site, df_proccessed, y_true, y_pred, plot_columns_x_label):
+
+    fig, axes = plt.subplots(1, len(plot_columns_x)-1, figsize=figsize, dpi=500, sharey=True)
+
+    for i, column in enumerate(plot_columns_x[1:-1]):
+        axes[i].plot(df_site[column].values,
+                      df_site[plot_columns_x[0]].values,
+                      label='Raw data',
+                      marker='o', color='k', linewidth = 0.2, markersize=0.8)
+
+        axes[i].set_ylim(ymin=0, ymax = 25)
+        axes[i].set_xlim(xmin=0)
+
+        axes[i].set_xlabel(plot_columns_x_label[i+1])
+        axes[i].grid(True, which='both', linestyle = 'dotted', linewidth = 0.3)
+        axes[i].legend(loc='lower center', handlelength=1.5, labelspacing=0.3)
+        axes[i].minorticks_on()
+
+
+    axes[0].set_ylabel('Depth [m]')
+
+    axes[-1].plot(y_pred,
+                df_site[plot_columns_x[0]].values,
+                label='ML output',
+                marker='o', color='blue', linewidth = 0.2, markersize=0.8)
+    axes[-1].plot(y_true,
+                  df_proccessed[plot_columns_x[0]].values,
+                  label='Raw data',
+                  marker='o', color='r', linewidth = 0.2, markersize=0.8)
+
+
+    axes[-1].set_xlabel(plot_columns_x_label[-1])
+    axes[-1].grid(True, which='both', linestyle = 'dotted', linewidth = 0.3)
+    axes[-1].legend(loc='lower center', handlelength=1.5, labelspacing=0.3)
     axes[-1].minorticks_on()
     axes[-1].invert_yaxis()
     axes[-1].set_xlim(xmin=0)
 
 
+    plt.subplots_adjust(wspace=0.0, hspace=0)
     plt.tight_layout()
+
     return fig, axes
+
+
 
 
 
@@ -228,8 +249,82 @@ def plot_cpt_data_ML_prediction(figsize, df_raw, df_SCPTu_SCPT, id_value, select
     plt.minorticks_on()
     #plt.ylim(ymin=0)
     plt.gca().invert_yaxis()
-    plt.grid(True, which='both')
+    plt.grid(True, which='both', linestyle = 'dotted', linewidth = 0.3)
 
     # Move the legend outside and to the top
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.17), ncol=1, handlelength = 3)
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.17), ncol=1, handlelength = 2, labelspacing=2)
     plt.tight_layout()
+
+
+import matplotlib
+import numpy as np
+
+
+def LegendVertical(Ax, Rotation=90, XPad=0, YPad=0, **LegendArgs):
+    if Rotation not in (90,270):
+        raise NotImplementedError('Rotation must be 90 or 270.')
+
+    # Extra spacing between labels is needed to fit the rotated labels;
+    # and since the frame will not adjust to the rotated labels, it is
+    # disabled by default
+    DefaultLoc = 'center left' if Rotation==90 else 'center right'
+    ArgsDefaults = dict(loc=DefaultLoc, labelspacing=3, frameon=False)
+    Args = {**ArgsDefaults, **LegendArgs}
+
+    Handles, Labels = Ax.get_legend_handles_labels()
+    Handles, Labels = Handles[-2:], Labels[-2:]
+    if Rotation==90:
+        # Reverse entries
+        Handles, Labels = (reversed(_) for _ in (Handles, Labels))
+    AxLeg = Ax.legend(Handles, Labels, **Args)
+
+    LegTexts = AxLeg.get_texts()
+    LegHandles = AxLeg.legend_handles
+
+    for L,Leg in enumerate(LegHandles):
+        if type(Leg) == matplotlib.patches.Rectangle:
+            BBounds = np.ravel(Leg.get_bbox())
+            BBounds[2:] = BBounds[2:][::-1]
+            Leg.set_bounds(BBounds)
+
+            LegPos = (
+                # Ideally,
+                #    `(BBounds[0]+(BBounds[2]/2)) - AxLeg.handletextpad`
+                # should be at the horizontal center of the legend patch,
+                # but for some reason it is not. Therefore the user will
+                # need to specify some padding.
+                (BBounds[0]+(BBounds[2]/2)) - AxLeg.handletextpad + XPad,
+
+                # Similarly, `(BBounds[1]+BBounds[3])` should be at the vertical
+                # top of the legend patch, but it is not.
+                (BBounds[1]+BBounds[3])+YPad
+            )
+
+        elif type(Leg) == matplotlib.lines.Line2D:
+            LegXY = Leg.get_xydata()[:,::-1]
+            Leg.set_data(*(LegXY[:,_] for _ in (0,1)))
+
+            LegPos = (
+                LegXY[0,0] - AxLeg.handletextpad + XPad,
+                max(LegXY[:,1]) + YPad
+            )
+
+        elif type(Leg) == matplotlib.collections.PathCollection:
+            LegPos = (
+                Leg.get_offsets()[0][0] + XPad,
+                Leg.get_offsets()[0][1] + YPad,
+            )
+        else:
+            raise NotImplementedError('Legends should contain Rectangle, Line2D or PathCollection.')
+
+        PText = LegTexts[L]
+        PText.set_verticalalignment('bottom')
+        PText.set_rotation(Rotation)
+        PText.set_x(LegPos[0])
+        PText.set_y(LegPos[1])
+
+        Lines = LegHandles[Leg]
+        Lines.set_x(LegPos[0])
+        Lines.set_y(LegPos[1])
+
+    return(None)
